bd8636702a7e204fae4fcf41c51db96895fa5d23
Jason Marshall <J.J.Marshall@kent.ac.uk>
2012-07-16 15:36:21 +0100

    fixed the bug with the logout view so that it will redirect to the token home page (which will then ask you to login again, but will then redirect to the home page on succesfull login)

--- usingWorkflows/urls.py
+++ usingWorkflows/urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^token/', include('tokens.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login_view'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='logout_view'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',{'next_page':'/token/'}, name='logout_view'),
 )
