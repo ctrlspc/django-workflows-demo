6c9fcccb37dfc45edb8e6e7de1d98da572998d1c
Jason Marshall <J.J.Marshall@kent.ac.uk>
2012-07-16 16:27:54 +0100

    removed the approved field as this is not nescessary, as the approved state is represented by a workflow state

--- tokens/models.py
+++ tokens/models.py
@@ -8,7 +8,6 @@ from workflows.utils import get_state
 class Token(models.Model):
     
     name = models.CharField(max_length=100)
-    approved = models.BooleanField(default=False)
     
     def __unicode__(self):
         return "%s, approved?:%s" % (self.name,self.approved )
