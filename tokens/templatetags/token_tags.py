ca9d6972c40fbf62f548a7ccb55689f5ffb1f937
Jason Marshall <ctrlspc@gmail.com>
2012-07-17 10:50:26 +0100

    created new templatetag
    this tests to see if a particular transition can be made, this makes
    more sense in the case of submission/approve/reject because these will
    cause transitions, so you should check that these transitions are
    possible first

--- tokens/templatetags/token_tags.py
+++ tokens/templatetags/token_tags.py
@@ -6,7 +6,7 @@ Created on 16 Jul 2012
 
 from django import template
 from permissions.utils import has_permission
-
+from workflows.utils import get_allowed_transitions
 register = template.Library()
 
 class HasPermissionForObjectNode(template.Node):
@@ -14,7 +14,7 @@ class HasPermissionForObjectNode(template.Node):
     
     @classmethod
     def handle_token(cls, parser, token):
-        bits = token.contents.split()
+        
         
         try:
             tag_name, object_under_test , permission = token.split_contents()
@@ -52,7 +52,11 @@ class HasPermissionForObjectNode(template.Node):
             if has_permission(object_under_Test, user, self.permission_string):
                 return self.nodelist_true.render(context)
             else:
-                return self.nodelist_false.render(context)
+                if isinstance(self.nodelist_false, str):
+                    return self.nodelist_false
+                else:
+                    return self.nodelist_false.render(context)
+                
             
         except template.VariableDoesNotExist:
             return ''
@@ -62,6 +66,64 @@ class HasPermissionForObjectNode(template.Node):
 def do_has_permission_for_object(parser, token):
     return HasPermissionForObjectNode.handle_token(parser, token)
 
+
+class CanMakeTransitionNode(template.Node):
+    
+    
+    @classmethod
+    def handle_token(cls, parser, token):
+        
+        try:
+            tag_name, object_under_test , transition = token.split_contents()
+        except ValueError:
+            raise template.TemplateSyntaxError("%r tag requires exactly two arguments" % tag_name)
+        
+        
+        end_tag = 'end_can_make_transition'
+        
+        nodelist_true = parser.parse(('else', end_tag)) # parse everythin between the strt tag and an else (if it exists) or the end tag
+        
+        token = parser.next_token() #parse the token 
+        
+        if token.contents == 'else': # there is an 'else' clause in the tag
+            nodelist_false = parser.parse((end_tag,)) # the contents of the else block if there is one
+            parser.delete_first_token()
+        else:
+            nodelist_false = ""
+
+        return cls(object_under_test , transition, nodelist_true, nodelist_false)
+    
+    
+    def __init__(self, object_under_test, transition_string, nodelist_true, nodelist_false):
+        self.object_under_Test = template.Variable(object_under_test)
+        self.transition_string = transition_string
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
+    
+    def render(self, context):
+        
+        try: 
+            object_under_Test = self.object_under_Test.resolve(context) #resolve the actual object from the context
+            user = context.get("user")
+            available_transitions = [transition.name for transition in get_allowed_transitions(object_under_Test, user)]
+            
+            
+            if self.transition_string in available_transitions:
+                return self.nodelist_true.render(context)
+            else:
+                if isinstance(self.nodelist_false, str):
+                    return self.nodelist_false
+                else:
+                    return self.nodelist_false.render(context)
+                
+            
+        except template.VariableDoesNotExist:
+            return ''
+    
+    
+@register.tag(name='can_make_transition')
+def do_can_make_transition(parser, token):
+    return CanMakeTransitionNode.handle_token(parser, token)
 
 
                             
