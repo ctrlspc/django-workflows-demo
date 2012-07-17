7c71d6292f4bc6c4b0a7dfc342a7d88d4bd9f67e
Jason Marshall <ctrlspc@gmail.com>
2012-07-17 11:50:33 +0100

    added some logic to get users for roles
    This allows you to get users for a role assigned to that object. I am
    not sure this is the best place to put the code, but it serves well as
    an example

--- tokens/models.py
+++ tokens/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from workflows.utils import get_state
+from permissions.models import  Role, PrincipalRoleRelation
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
 
 
@@ -14,7 +16,19 @@ class Token(models.Model):
     
     def get_current_state(self):
         return get_state(self)
+    
+    def get_token_generators(self):
+        token_generator_role = Role.objects.get(name='Token_Generator')
+        return [roleRelation.user for roleRelation in self._get_role_relations_for_me().filter(role=token_generator_role) ]
+    
+    def get_token_supervisor(self):
         
+        supervisor_role = Role.objects.get(name='Supervisor')
+        return [roleRelation.user for roleRelation in self._get_role_relations_for_me().filter(role=supervisor_role) ]
+    
+    def _get_role_relations_for_me(self):
+        ctype = ContentType.objects.get_for_model(self)
+        return PrincipalRoleRelation.objects.filter(content_type=ctype).filter(content_id=self.id)
     
     
 class SupervisorRelationship(models.Model):
